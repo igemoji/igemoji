4241935bc578c32c761f2f0e9128305bff0ab275
이재종 <wowhd45@naver.com>
2024-04-23 16:48:03 +0900

    Fix: 충돌 해결

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,8 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-<<<<<<< HEAD
-=======
     plugins: [
       [
         "module:react-native-dotenv",
@@ -17,6 +15,5 @@ module.exports = function (api) {
         },
       ],
     ],
->>>>>>> c0601b32f54267f9fb89e65232d4c24d93f64750
   };
 };
