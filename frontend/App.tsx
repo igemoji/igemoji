bd0a056c4e967d4a633eae9f6281bd33390844e1
이재종 <wowhd45@naver.com>
2024-05-03 11:22:29 +0900

    [#135] Fix: 빌드 에러 수정

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,29 +1,27 @@
-import { NavigationContainer, useNavigation, useIsFocused } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Audio } from "expo-av";
 import Constants from "expo-constants";
 import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
-import * as React from "react";
+import { useEffect, useState } from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
-
-import SignIn from "./src/screens/Auth/SignIn";
-import SignUp from "./src/screens/Auth/SignUp";
-import Game from "./src/screens/Game/Game";
-import Main from "./src/screens/Main/Main";
-import RoomList from "./src/screens/Main/RoomList";
-import Rank from "./src/screens/Rank/Rank";
 
 import { MusicContext } from "@/config/Music";
 import { themes, ThemeContext } from "@/config/Theme";
+import SignIn from "@/screens/Auth/SignIn";
+import SignUp from "@/screens/Auth/SignUp";
+import Game from "@/screens/Game/Game";
+import Main from "@/screens/Main/Main";
+import RoomList from "@/screens/Main/RoomList";
+import Rank from "@/screens/Rank/Rank";
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-  const [theme, setTheme] = React.useState(themes.light);
-  const [sound, setSound] = React.useState<Audio.Sound | undefined>(undefined);
-  const [buttonSound, setButtonSound] = React.useState<Audio.Sound | undefined>(undefined);
-  const [isMusicOn, setIsMusicOn] = React.useState(true);
+  const [theme, setTheme] = useState(themes.light);
+  const [sound, setSound] = useState<Audio.Sound | undefined>(undefined);
+  const [buttonSound, setButtonSound] = useState<Audio.Sound | undefined>(undefined);
+  const [isMusicOn, setIsMusicOn] = useState(true);
 
   const toggleTheme = () => {
     const newTheme = theme === themes.light ? themes.dark : themes.light;
@@ -71,14 +69,11 @@ function App() {
     }
   }
 
-  React.useEffect(() => {
-    SplashScreen.preventAutoHideAsync();
+  useEffect(() => {
     loadSound();
   }, []);
 
-  const [fontsLoaded] = useFonts({
-    Katuri: require("~/fonts/Katuri.ttf"),
-    Dovemayo: require("~/fonts/Dovemayo.ttf"),
+  const [fontsLoaded, fontError] = useFonts({
     PretendardBlack: require("~/fonts/Pretendard-Black.ttf"),
     PretendardBold: require("~/fonts/Pretendard-Bold.ttf"),
     PretendardExtraBold: require("~/fonts/Pretendard-ExtraBold.ttf"),
@@ -90,33 +85,26 @@ function App() {
     PretendardThin: require("~/fonts/Pretendard-Thin.ttf"),
   });
 
-  React.useEffect(() => {
-    // console.log(fontsLoaded);
-    // console.log(sound);
-
-    if (fontsLoaded && sound) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, sound]);
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
 
   return (
-    fontsLoaded && (
-      <ThemeContext.Provider value={{ theme, toggleTheme }}>
-        <MusicContext.Provider value={{ isMusicOn, toggleMusic, sound, setSound, playButtonSound }}>
-          <SafeAreaView style={styles.screen} />
-          <NavigationContainer>
-            <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="SignIn">
-              <Stack.Screen name="SignIn" component={SignIn} />
-              <Stack.Screen name="SignUp" component={SignUp} />
-              <Stack.Screen name="Game" component={Game} />
-              <Stack.Screen name="Main" component={Main} />
-              <Stack.Screen name="RoomList" component={RoomList} />
-              <Stack.Screen name="Rank" component={Rank} />
-            </Stack.Navigator>
-          </NavigationContainer>
-        </MusicContext.Provider>
-      </ThemeContext.Provider>
-    )
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <MusicContext.Provider value={{ isMusicOn, toggleMusic, sound, setSound, playButtonSound }}>
+        <SafeAreaView style={styles.screen} />
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="SignIn">
+            <Stack.Screen name="SignIn" component={SignIn} />
+            <Stack.Screen name="SignUp" component={SignUp} />
+            <Stack.Screen name="Game" component={Game} />
+            <Stack.Screen name="Main" component={Main} />
+            <Stack.Screen name="RoomList" component={RoomList} />
+            <Stack.Screen name="Rank" component={Rank} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </MusicContext.Provider>
+    </ThemeContext.Provider>
   );
 }
 
