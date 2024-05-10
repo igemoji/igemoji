78dad72abbeb88f6750f415c9de137909b1a595b
서성원 <zsa332@gmail.com>
2024-05-10 10:57:09 +0900

    FEAT : 맴버 상세 조회 & 회원 탈퇴 #S10P31A605-174

--- backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/member/service/MemberService.java
+++ backend/Igemoji/src/main/java/com/ssafy/igemoji/domain/member/service/MemberService.java
@@ -1,6 +1,10 @@
 package com.ssafy.igemoji.domain.member.service;
 
+import com.ssafy.igemoji.domain.level.Level;
+import com.ssafy.igemoji.domain.level.exception.LevelErrorCode;
+import com.ssafy.igemoji.domain.level.repository.LevelRepository;
 import com.ssafy.igemoji.domain.member.Member;
+import com.ssafy.igemoji.domain.member.dto.MemberInfoResponseDto;
 import com.ssafy.igemoji.domain.member.dto.MemberRankDto;
 import com.ssafy.igemoji.domain.member.dto.NicknameRequestDto;
 import com.ssafy.igemoji.domain.member.dto.RankResponseDto;
@@ -19,7 +23,9 @@ import java.util.List;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final LevelRepository levelRepository;
 
+    /* 닉네임 수정 */
     @Transactional
     public void updateNickname(NicknameRequestDto requestDto){
         Member member = memberRepository.findById(requestDto.getMemberId()).orElseThrow(
@@ -32,6 +38,7 @@ public class MemberService {
         memberRepository.save(member);
     }
 
+    /* 랭킹 조회 */
     public RankResponseDto getRank(Integer memberId){
         Member member = memberRepository.findById(memberId).orElseThrow(
                 () -> new CustomException(MemberErrorCode.NOT_FOUND_MEMBER)
@@ -48,8 +55,31 @@ public class MemberService {
         return new RankResponseDto(myRank, memberRankDtoList);
     }
 
+    /* 닉네임 중복 체크 */
     public boolean exitNickname(String nickname) {
         return memberRepository.existsByNickname(nickname);
     }
+
+    /* 맴버 조회 */
+    public MemberInfoResponseDto getMember(Integer memberId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new CustomException(MemberErrorCode.NOT_FOUND_MEMBER));
+
+        Level level = levelRepository.findById(member.getLevel())
+                .orElseThrow(() -> new CustomException(LevelErrorCode.NOT_FOUND_LEVEL));
+
+        int rank = memberRepository.getMemberRank(member.getRating());
+        return MemberInfoResponseDto.toDto(member, rank + 1, level.getExp());
+    }
+
+
+    /* 회원 탈퇴 */
+    public void removeMember(Integer memberId) {
+        if(memberRepository.existsById(memberId))
+            throw new CustomException(MemberErrorCode.NOT_FOUND_MEMBER);
+
+        memberRepository.deleteById(memberId);
+    }
+
 }
 
